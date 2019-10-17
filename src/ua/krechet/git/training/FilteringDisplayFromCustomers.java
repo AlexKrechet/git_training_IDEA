584475086b600d21f10e2152d562dbffa7663b61
Alex Krechet <rubassabaka88@gmail.com>
2019-10-17 17:23:02 +0300

    List customer type filled

--- src/ua/krechet/git/training/FilteringDisplayFromCustomers.java
+++ src/ua/krechet/git/training/FilteringDisplayFromCustomers.java
@@ -3,7 +3,7 @@ package ua.krechet.git.training;
 import java.util.List;
 
 public class FilteringDisplayFromCustomers {
-    public void show(List customers) {
+    public void show(List<Customer> customers) {
         customers.stream().filter(customer -> customer.getAge() > 25).sorted((c1, c2) -> c1.getName().compareTo(c2.getName()))
                 .forEach(System.out::println);
     }
